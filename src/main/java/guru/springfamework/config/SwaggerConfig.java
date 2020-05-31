c65aa5e0718e0b27bf01c16d2a85aee579aad285
Nilvandro <nbcmuianga@gmail.com>
2020-05-31 07:45:10 +0200

    Customizing Swagger with metadata

--- src/main/java/guru/springfamework/config/SwaggerConfig.java
+++ src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -2,18 +2,24 @@ package guru.springfamework.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 /**
  * Created by Nilvandro Muianga on 5/31/2020
  */
 @EnableSwagger2
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig {//} extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket api() {
@@ -22,6 +28,25 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
     }
+
+    private ApiInfo metaData() {
+        Contact contact = new Contact("Nilvandro Muianga",
+                "https://www.google.com", "nbcmuianga@gmail.com");
+
+        return new ApiInfo("Spring Framework Guru", "Spring Framework 5: Beginner to Guru", "1.0",
+                "Terms od Service: ", contact, "Apache License version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0", new ArrayList<>());
+    }
+
+   /* @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars");
+    }*/
 }
